bc2b35e60b49870a36060bf8c22e33ce2bbadc2d
Nishantgupta6688 <94850930+Nishantgupta6688@users.noreply.github.com>
2022-07-25 11:34:04 +0530

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://Nishantgpt619:NishantWorkPermit@workpermit.oqqm4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv", { useNewUrlParser: true, useUnifiedTopology: true });
 
 var conn = mongoose.connection;
 
